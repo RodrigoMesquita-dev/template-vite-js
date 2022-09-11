e3dccb55d47edfe23fd3c6f645b576f7626fdee0
rodrigomesquita-dev <rodrigomesquita9232@gmail.com>
2022-09-11 01:57:37 -0400

    landing page basics finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,17 +9,82 @@
             <p class="mb-10"> 
               I'm a developer
             </p>
-            <a class="bg-pink-500 rounded cursor-pointer py-2 px-4 text-white w-max hover:bg-pink-800">
+            <a class=" button w-max">
               Checkout my work
             </a>
           </div>  
         </div>
       </div>
-      <!--
-      <div>
-        <img src="">
+      <div class="wrapper bg-form-pattern">
+        <h2>Portfolio</h2>
+        <p class="" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam dignissim felis vitae ornare. 
+          Phasellus dictum placerat convallis. Aenean fermentum vulputate massa. Fusce vitae mi commodo, consequat nisl nec, 
+          iaculis nulla. Proin tincidunt vehicula erat, eu dictum sem mollis ut. Suspendisse posuere elit in dui bibendum condimentum 
+          sit amet at ligula. Fusce tincidunt dapibus viverra. In iaculis lacus vitae finibus efficitur. Suspendisse tristique purus
+          nec nulla tempor, ut porttitor est blandit. Integer interdum cursus commodo. Cras pharetra libero leo, in auctor est
+          vehicula scelerisque. Ut dapibus elementum magna, eu bibendum lacus aliquet rutrum. Nam volutpat 
+          tempor lacus, sed volutpat quam imperdiet sit amet. Quisque sed tortor non nisl varius scelerisque ut vel lorem. Aliquam at urna 
+          vitae metus maximus tempor.</p>
+        <div class="grid grid-cols-2 gap-8 my-8">
+          <div class="col-span-1 bg-portfolio-work-background bg-no-repeat bg-center py-8">
+            <img class="w-495 h-285 rounded mx-auto " src="../images/portfolio-1.png" alt=""/>
+          </div>
+          <div class="col-span-1 bg-portfolio-work-background bg-no-repeat bg-center py-8">
+            <img class="w-495 h-285 rounded mx-auto" src="../images/portfolio-1.png" alt=""/>
+          </div>
+        </div>
       </div>
-      -->
+      <div class="wrapper bg-about-pattern">
+        <h2>About</h2>
+        <div class="flex flex-row items-center">
+          <div class=" flex-1 bg-portfolio-work-background bg-no-repeat bg-center py-8">
+            <img class="w-495 h-285 rounded mx-auto " src="../images/portfolio-1.png" alt=""/>
+          </div>
+          <p class="" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam dignissim felis vitae ornare. 
+          Phasellus dictum placerat convallis. Aenean fermentum vulputate massa. Fusce vitae mi commodo, consequat nisl nec, 
+          iaculis nulla. Proin tincidunt vehicula erat, eu dictum sem mollis ut. Suspendisse posuere elit in dui bibendum condimentum 
+          sit amet at ligula. Fusce tincidunt dapibus viverra. In iaculis lacus vitae finibus efficitur. Suspendisse tristique purus
+          nec nulla tempor, ut porttitor est blandit. Integer interdum cursus commodo. Cras pharetra libero leo, in auctor est
+          vehicula scelerisque. Ut dapibus elementum magna, eu bibendum lacus aliquet rutrum. Nam volutpat 
+          tempor lacus, sed volutpat quam imperdiet sit amet. Quisque sed tortor non nisl varius scelerisque ut vel lorem. Aliquam at urna 
+          vitae metus maximus tempor.</p>
+        </div>
+      </div>
+      <div class="wrapper bg-form-pattern">
+        <h2>Contact me</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam dignissim felis vitae ornare. 
+          Phasellus dictum placerat convallis. Aenean fermentum vulputate massa. Fusce vitae mi commodo, consequat nisl nec, 
+          iaculis nulla. Proin tincidunt vehicula erat, eu dictum sem mollis ut. Suspendisse posuere elit in dui bibendum condimentum 
+          sit amet at ligula. Fusce tincidunt dapibus viverra. In iaculis lacus vitae finibus efficitur. Suspendisse tristique purus
+          nec nulla tempor, ut porttitor est blandit. Integer interdum cursus commodo.
+        </p>
+        <form method="POST" class="grid grid-cols-2 gap-8 mt-32">
+          <label for="email" class="col-span-1">
+            Email
+            <input name="email"/>
+          </label>
+          <label for="honeypot" class="col-span-1">
+            <input type="hidden" name="honeypot"/>
+          </label>
+          <label for="firstname" class="col-span-1">
+            FirstName
+            <input name="firstname" />
+          </label>
+          <label for="lastname" class="col-span-1">
+            Lastname
+            <input name="lastname"/>
+          </label>
+          <label for="message" class="col-span-2">
+            Message
+            <textarea name="message">
+            </textarea>
+          </label>
+          <button type="submit" class="col-span-2 button">
+            Contact-me
+          </button>
+        </form>
+      </div>
     </main>
 </template>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,15 +6,28 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('./images/hero-pattern.png')"
+        'hero-pattern': "url('./images/hero-pattern.png')",
+        'about-pattern': "url('./images/about-pattern.png')",
+        'form-pattern': "url('./images/form-pattern.png')",
+        'portfolio-work-background': "url('./images/portfolio-work-background.svg')"
       },
       colors:{
         'primary': '#9B59B6', // <div class='bg-tutorial-green'> pra chamar por exemplo.
-        'secondary': '#6C6C6C'
+        'secondary': '#6C6C6C',
+        'formBackground': '#73D48C',
+        'formFieldBorder': '#26547C',
+      },
+      width: {
+        '495': '459px'
+      },
+
+      height: {
+        '285': '285px'
       }
-    },
-    
+      
+    },    
   },
+
   variants: {
     extend: {},
   },
